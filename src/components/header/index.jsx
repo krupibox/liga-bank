49320418a01e9d6088251075d268002319893cfe
Ilya Krupin <dev@krupibox.ru>
2021-03-11 18:31:45 +0300

    Add header

---
+++ src/components/header/index.jsx
@@ -0,0 +1,18 @@
+
+import { menuItems } from "../../helpers";
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+import styles from "./Header.module.css";
+
+const Header = () =>
+(
+    <div className={styles.navigation}>
+        <Logo />
+        <nav className={styles.navigation}>{
+            menuItems && menuItems.map((item) => (<a className={styles.menuLink} key={item} href="/">{item}</a>))
+        }</nav>
+
+        <div>Войти в Интернет-банк</div>
+    </div>
+)
+
+export default Header;
