21684e82dc9178e4416ea63b04d32d1d8d9a8c8c
Ilya Krupin <dev@krupibox.ru>
2021-03-12 19:11:09 +0300

    Set state cyrrency

--- src/components/converter/index.jsx
+++ src/components/converter/index.jsx
@@ -1,22 +1,49 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import styles from "./Converter.module.scss";
 
-const Converter = () =>
-(
-    <div className={styles.container}>
+const Converter = () => {
+    const [startDate, setStartDate] = useState(new Date());
+    const [currencyFrom, setCurrencyFrom] = useState(0);
+    const [currencyTo, setCurrencyTo] = useState(0);
+
+    const handleSubmit = () => {
+        console.log(currencyFrom * currencyTo);
+    }
+
+    const handleChangeFrom = (event) => setCurrencyTo(event.target.value);
+    const handleChangeTo = (event) => setCurrencyFrom(event.target.value);
+
+    console.log(currencyFrom);
+
+    return (<div className={styles.container}>
         <h2>Конвертер валют</h2>
-        <fieldset >
-            <legend>У меня есть:</legend>
+        <form onSubmit={handleSubmit}>
+            <fieldset >
+                <div>
+                    <legend>У меня есть:</legend>
+                    <input type="number" name="from" defaultValue={currencyFrom} onChange={handleChangeFrom} />
+                    <select>
+                        <option value="rub">RUB</option>
+                        <option value="usd">USD</option>
+                    </select>
+                </div>
+                <div>
+                    <legend>Хочу приобрести:</legend>
+                    <input type="number" name="to" defaultValue={currencyTo} onChange={handleChangeTo} />
+                    <select>
+                        <option value="rub">USD</option>
+                        <option value="usd">RUB</option>
+                    </select>
+                </div>
+            </fieldset>
             <div>
-                <input type="text" name="name" placeholder="1000" size="8" required />
-                <select>
-                    <option value="grapefruit">USD</option>
-                    <option value="lime">RUB</option>
-                    <option selected value="coconut">CHF</option>
-                    <option value="mango">EUR</option>
-                </select>
+                <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+                <button type="submit">Сохранить результат </button>
             </div>
-        </fieldset>
-    </div>
-)
+        </form>
+    </div>)
+}
 
 export default Converter;
