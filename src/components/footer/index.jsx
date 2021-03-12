d688fdfccfe5dd41fd64fd1e0f2fc285f420136d
Ilya Krupin <dev@krupibox.ru>
2021-03-12 17:09:38 +0300

    Update footer styles

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -8,24 +8,25 @@ const ITEM = 2;
 const Footer = () =>
 (
     <div className={styles.wrapper}>
-        <div className={[styles.col, styles.logoInner].join(" ")}>
+        <div className={[styles.column, styles.logoInner].join(" ")}>
             <a href="/"><Logo className={styles.logoIcon} /></a>
             <p className={styles.address}>150015, г. Москва, ул. Московская, д. 32 Генеральная лицензия Банка России №1050 Ⓒ Лига Банк, 2019</p>
         </div>
-        <ul className={[styles.col, styles.menuList].join(" ")}>
+        <ul className={[styles.column, styles.menuInner].join(" ")}>
             {removeItem(menuItems, ITEM).map(item => <li key={item}><a href="/">{item}</a></li>)}
         </ul>
-        <div className={styles.col}>
-            <div className={styles.mobile}><span className={styles.accent}>*0904</span>
+        <div className={[styles.column, styles.phonesInner].join(" ")}>
+            <div className={styles.mobile}>
+                <span className={styles.accent}>*0904</span>
             Бесплатно для абонентов МТС, Билайн, Мегафон, Теле2
             </div>
-        </div>
-        <div className={styles.col}>
-            <div className={styles.handset}><span className={styles.accent}>8 800 111 22 33</span>
+
+            <div className={styles.handset}>
+                <span className={styles.accent}>8 800 111 22 33</span>
             Бесплатный для всех городов России
             </div>
         </div>
-        <div className={[styles.col, styles.social].join(" ")}>
+        <div className={[styles.column, styles.socialInner].join(" ")}>
             <ul className={styles.socialList}>
                 {
                     footerSocialItems.map((item => <li key={item.name}>
